416ab454bf7c6e9be7925a6fe394387c028232cc
Kadir Esen <kadir@kadiresen.com>
2026-01-03 09:34:16 +0300

    Add Flathub packaging and MPRIS init

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,6 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
+  let mut builder = tauri::Builder::default()
     .plugin(tauri_plugin_os::init())
     .setup(|app| {
       if cfg!(debug_assertions) {
@@ -11,7 +11,13 @@ pub fn run() {
         )?;
       }
       Ok(())
-    })
-    .run(tauri::generate_context!())
+    });
+
+  #[cfg(target_os = "linux")]
+  {
+    builder = builder.plugin(tauri_plugin_mpris::init());
+  }
+
+  builder.run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
